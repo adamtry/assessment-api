71b530a0994877b07ab397beb81bacb17f1976d8
Adam <adam.tracy20@gmail.com>
2023-11-17 19:03:14 +0000

    psycopg2-fix

--- tests/gateways/test_address_gateway.py
+++ tests/gateways/test_address_gateway.py
@@ -22,47 +22,48 @@ def address():
     """Address entity fixture with fake data"""
     # TODO: Use autofixture equivalent to generate fake data
     return sql_schema.HackneyAddressEntity(
-            lpi_key=faker.pystr(),
-            lpi_logical_status=faker.pystr(),
-            lpi_start_date=faker.pyint(),
-            lpi_end_date=faker.pyint(),
-            lpi_last_update_date=faker.pyint(),
-            usrn=faker.pyint(),
-            uprn=faker.pyint(),
-            parent_uprn=faker.pyint(),
-            blpu_start_date=faker.pyint(),
-            blpu_end_date=faker.pyint(),
-            blpu_class=faker.pystr()[0:4],
-            blpu_last_update_date=faker.pyint(),
-            usage_description=faker.pystr(),
-            usage_primary=faker.pystr(),
-            property_shell=faker.pybool(),
-            easting=faker.pyfloat(),
-            northing=faker.pyfloat(),
-            unit_number=faker.pyint(),
-            sao_text=faker.pystr(),
-            building_number=faker.pystr(),
-            pao_text=faker.pystr(),
-            paon_start_num=faker.pyint(),
-            street_description=faker.pystr(),
-            locality=faker.pystr(),
-            ward=faker.pystr(),
-            town=faker.pystr(),
-            postcode=faker.postcode(),
-            postcode_nospace=faker.postcode(),
-            planning_use_class=faker.pystr(),
-            neverexport=faker.pybool(),
-            longitude=faker.pyfloat(),
-            latitude=faker.pyfloat(),
-            gazetteer=faker.pystr(),
-            organisation=faker.pystr(),
-            line1=faker.pystr(),
-            line2=faker.pystr(),
-            line3=faker.pystr(),
-            line4=faker.pystr()
-        )
+        lpi_key=faker.pystr(),
+        lpi_logical_status=faker.pystr(),
+        lpi_start_date=faker.pyint(),
+        lpi_end_date=faker.pyint(),
+        lpi_last_update_date=faker.pyint(),
+        usrn=faker.pyint(),
+        uprn=faker.pyint(),
+        parent_uprn=faker.pyint(),
+        blpu_start_date=faker.pyint(),
+        blpu_end_date=faker.pyint(),
+        blpu_class=faker.pystr()[0:4],
+        blpu_last_update_date=faker.pyint(),
+        usage_description=faker.pystr(),
+        usage_primary=faker.pystr(),
+        property_shell=faker.pybool(),
+        easting=faker.pyfloat(),
+        northing=faker.pyfloat(),
+        unit_number=faker.pyint(),
+        sao_text=faker.pystr(),
+        building_number=faker.pystr(),
+        pao_text=faker.pystr(),
+        paon_start_num=faker.pyint(),
+        street_description=faker.pystr(),
+        locality=faker.pystr(),
+        ward=faker.pystr(),
+        town=faker.pystr(),
+        postcode=faker.postcode(),
+        postcode_nospace=faker.postcode(),
+        planning_use_class=faker.pystr(),
+        neverexport=faker.pybool(),
+        longitude=faker.pyfloat(),
+        latitude=faker.pyfloat(),
+        gazetteer=faker.pystr(),
+        organisation=faker.pystr(),
+        line1=faker.pystr(),
+        line2=faker.pystr(),
+        line3=faker.pystr(),
+        line4=faker.pystr(),
+    )
 
 
+# TODO: Fix psycopg2 cross-platform issue
 @pytest.fixture
 def create_test_data(connection_string, address):
     # Create test data
@@ -107,4 +108,3 @@ def test_get_addresses_no_results(address, connection_string):
 
     # Assert
     assert len(addresses) == 0
-
