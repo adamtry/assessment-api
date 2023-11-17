2859282ef21377c1814978313008d7fb965de27c
Adam <adam.tracy20@gmail.com>
2023-11-17 15:31:17 +0000

    Refactored controller to new file

--- app.py
+++ app.py
@@ -1,38 +1,9 @@
-import re
 from flask import Flask
-
-from src.gateways.address_gateway import AddressGateway
-from src.use_cases.get_addresses_use_case import GetAddressesUseCase
-from urllib.parse import unquote
+from src.controllers.address_controller import address_controller
 
 
 app = Flask(__name__)
-
-
-_username, _password, _host, _port = ("postgres", "mypassword", "localhost", 5432)
-connection_string = f"postgresql://{_username}:{_password}@{_host}:{_port}/postgres"
-ADDRESS_GATEWAY = AddressGateway(connection_string)
-GET_ADDRESSES_USE_CASE = GetAddressesUseCase(ADDRESS_GATEWAY)
-
-
-POSTCODE_REGEX = r"[A-Za-z][1-9] [1-9][A-Za-z]{2}"
-
-
-@app.route('/addresses/<postcode>', methods=['GET'])
-def get_addresses_for_postcode(postcode: str):
-    postcode = unquote(postcode).strip()
-    if not postcode:
-        return "No postcode provided", 400
-    if not re.match(POSTCODE_REGEX, postcode):
-        return f"Validation error: Postcode {postcode} is not valid", 400
-    addresses = GET_ADDRESSES_USE_CASE.execute(postcode)
-    if not addresses:
-        return f"No addresses found for postcode {postcode}", 404
-    return addresses, 200
-
-# @app.route('/addresses/<postcode>', methods=['GET'])
-# def test_get_postcode(postcode):
-#     return f"Postcode is {postcode}", 200
+app.register_blueprint(address_controller)
 
 
 if __name__ == '__main__':
